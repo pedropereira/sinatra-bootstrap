77eef684fc9816f5b519bf4ddfd21473226d8bba
Pedro Pereira <pedro.mdp@gmail.com>
2012-07-12 03:33:50 +0100

    added project files

---
+++ app.rb
@@ -0,0 +1,14 @@
+require 'sinatra'
+require 'sass'
+
+set :sass, :style => :compressed
+
+get '/stylesheets/:filename.css' do
+  content_type 'text/css', :charset => 'utf-8'
+  filename = "#{params[:filename]}"
+  render :sass, filename.to_sym, :views => './views/stylesheets'
+end
+
+get '/' do
+  erb :index
+end
